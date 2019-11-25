f673a7e22a43d8cf1180afc95f63219396663bdf
lijutsang <957790617@qq.com>
2019-11-25 10:13:09 +0800

    commit

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2019/11/25 10:10:19
+@Author  :   LiJu Tsang
+@Version :   1.0
+@Contact :   zengliju60@gmail.com,957790617@qq.com
+@Desc    :   None
+'''
+
+# here put the import code
